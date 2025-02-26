055f9ce11d15693374e868cdb3bccd3db633848b
Abraham Haile Araya <107077258+abrshz@users.noreply.github.com>
2025-02-26 11:59:58 +0300

    Day 6 Functions

---
+++ Day 6/daySix.py
@@ -0,0 +1,29 @@
+# Functions 
+
+a = 4
+b = 5
+
+sum = a + b
+
+print(sum)
+
+def mani():
+    num1 = int(input("Give number 1 \n"))
+    num2 = int(input("Give number 2 \n"))
+
+    sum = num1 + num2
+
+    print(f"\nThe sum of {num1} and {num2} is: {sum}")
+
+mani()
+
+
+
+
+
+
+
+
+
+
+
