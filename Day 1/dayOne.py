652e91984a3e2894e35724821fda055f5308c96b
abrshz <trusewynur@gmail.com>
2026-01-19 22:37:59 +0300

    Update day 1 challenge

--- Day 1/dayOne.py
+++ Day 1/dayOne.py
@@ -1,4 +1,28 @@
+# String Manipulation:- learn to use string concatenation and the new line escape sequence to format strings in Python. 
+# \n use for new line 
+#  + sign use for concatenate strings to merge in one line 
+
+# Examples
 print("Welcome to the Band Name Generator.")
+
+# Inputs learn to use the Python input() functions to collect user input and use it within your code.
+
+# Examples
+
+print('Hello ' + input('What is your name?' + '!'))
+
+# Variables to store values in containers for later use. Variables in a concept in programming that allows us to give label to a piece of data so that we can refer or reference that data using the chosen variable name. We will see in this lesson how to create variable ans how to use the variable to access the contained value. 
+
+# Examples
+
 name = input("What's the name of the city you grew up in?\n")
 petName = input("What's your pet's name?\n")
 print("Your band name could be " + name + " " + petName + "!")
+
+# len() function is use for calculate the length.
+
+print(len(name))
+
+
+
+
