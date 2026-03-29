e21d63876b57ed2069168fbb1b31b3ebc59b23c3
abrshz <trusewynur@gmail.com>
2026-03-29 16:30:24 +0300

    Created quize file and add some image to project file

--- Day 3/projectDay3.py
+++ Day 3/projectDay3.py
@@ -1,14 +1,42 @@
+print(''' 
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/[TomekK]
+*******************************************************************************
+ ''')
+
+
 print("Welcome to Treasure Island.")
 print("Your mission is to find the hidden treasure.")
 
 choice = input("You are standing at a crossroad. \nType 'left' or 'right': ").lower()
 
 if choice == "left":
-    action = input("You've come to a lake. There's an island in the middle. \nType 'wait' to wait for a boat or 'swim' to swim across: ")
-    
+    action = input(
+        "You've come to a lake. There's an island in the middle. \nType 'wait' to wait for a boat or 'swim' to swim across: "
+    )
+
     if action == "wait":
-        color = input("You arrive at the island safely. \nChoose a color to get prize: 'Red', 'Blue', or 'Yellow': ")
-        
+        color = input(
+            "You arrive at the island safely. \nChoose a color to get prize: 'Red', 'Blue', or 'Yellow': "
+        )
+
         if color == "Red":
             print("Burned by fire.\nGame over!!")
         elif color == "Blue":
