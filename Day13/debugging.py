7ff0bd09036b54ad9018fea456b0ec9faf0237d3
abrshz <trusewynur@gmail.com>
2026-04-13 23:34:05 +0300

    Done day 13

---
+++ Day13/debugging.py
@@ -0,0 +1,14 @@
+from random import randint
+def my_function():
+    for i in range (1,21):
+        # print(i)
+        if i == 20:
+            # print(i)
+            print('You got it!')
+my_function()
+
+dice_images = ['1', '2', '3', '4', '5', '6']
+dice_num = randint(0, 5)
+print(dice_images[dice_num])
+
+
